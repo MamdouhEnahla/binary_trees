576369fc3df2dc4df4defe7c26330ee3eb481c3d
MamdouhEnahla <mamdouh57118@gmail.com>
2024-03-28 05:30:50 +0200

    9-and()...

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (!tree || !tree->left && !tree->right)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 	left_height += binary_tree_height(tree->left);
 	right_height += binary_tree_height(tree->right);
