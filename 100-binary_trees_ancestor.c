8c7549d7a729dca638c833a84867c7e37033541c
MamdouhEnahla <mamdouh57118@gmail.com>
2024-03-29 01:26:01 +0200

    100...

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ *
+ * Return: Null if uncle not found
+ * else, pointer to uncle Node
+*/
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second
+)
+{
+	const binary_tree_t *temp_s;
+	const binary_tree_t *temp_f;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	if (second->parent == first)
+		return (second->parent);
+	temp_f = first;
+	temp_s = second;
+
+	while (temp_f->parent)
+	{
+		while (temp_s->parent)
+		{
+			if (temp_s->parent == temp_f->parent)
+				return (temp_s->parent);
+			temp_s = temp_s->parent;
+		}
+		temp_f = temp_f->parent;
+	}
+	return (NULL);
+}
