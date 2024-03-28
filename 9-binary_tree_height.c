9c59049bd541977ebdc66f3ee1ad6d8d6228b99e
MamdouhEnahla <mamdouh57118@gmail.com>
2024-03-28 02:56:00 +0200

    fix-height-typos...

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree from node. 0 if tree is NULL
- */
+* binary_tree_height - Measures the height of a binary tree
+* @tree: Pointer to the root node of the tree to measure the height
+*
+* Return: The height of the tree from node. 0 if tree is NULL
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
@@ -18,5 +18,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height += binary_tree_height(tree->left);
 	right_height += binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
